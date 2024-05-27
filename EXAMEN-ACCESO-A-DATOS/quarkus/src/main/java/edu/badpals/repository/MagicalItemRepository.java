d61ad53de2c4e8aa4eab6d4967b91b90ed004f65
bestiadivina147 <diegopc147@gmail.com>
2024-05-27 10:27:21 +0200

    style:

--- EXAMEN-ACCESO-A-DATOS/quarkus/src/main/java/edu/badpals/repository/MagicalItemRepository.java
+++ EXAMEN-ACCESO-A-DATOS/quarkus/src/main/java/edu/badpals/repository/MagicalItemRepository.java
@@ -3,6 +3,6 @@ import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 import edu.badpals.dominio.MagicalItem;
 
-public class MagicalItemRepository implements PanacheRepositoryBase<Item, String>{
+public class MagicalItemRepository implements PanacheRepositoryBase<MagicalItem, String>{
     
 }
