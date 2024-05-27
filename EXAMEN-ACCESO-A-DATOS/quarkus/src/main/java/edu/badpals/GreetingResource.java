ec64330c92a1338e1a7d74f837e0062f35fe15c4
bestiadivina147 <diegopc147@gmail.com>
2024-05-27 10:46:07 +0200

    build(ServiceOlli.java): add ServiceOlli.java

--- EXAMEN-ACCESO-A-DATOS/quarkus/src/main/java/edu/badpals/GreetingResource.java
+++ EXAMEN-ACCESO-A-DATOS/quarkus/src/main/java/edu/badpals/GreetingResource.java
@@ -1,9 +1,19 @@
 package edu.badpals;
 
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.List;
+import edu.badpals.dominio.*;
 
 @Path("/hello")
 public class GreetingResource {
--- EXAMEN-ACCESO-A-DATOS/quarkus/src/main/java/edu/badpals/ServiceOlli.java
+++ EXAMEN-ACCESO-A-DATOS/quarkus/src/main/java/edu/badpals/ServiceOlli.java
@@ -12,6 +12,79 @@ import edu.badpals.dominio.Order;
 import edu.badpals.repository.MagicalItemRepository;
 import edu.badpals.repository.OrderRepository;
 import edu.badpals.repository.WizardsRepository;
+
+@ApplicationScoped
 public class ServiceOlli {
+    @Inject
+    public WizardsRepository wizardsRepo;
+
+    @Inject
+    public MagicalItemRepository itemRepo;
+
+    @Inject
+    public OrderRepository orderRepo;
+
+    public ServiceOlli() {}
     
+    public Wizards cargaWizard(String nombre) {
+        Optional<Wizards> wizards = wizardsRepo.findByIdOptional(nombre);
+        return wizards.isPresent()? wizards.get(): new Wizards();
+    }
+
+    public MagicalItem cargaMagicalItem(String nombre) {
+        Optional<MagicalItem> item = itemRepo.findByIdOptional(nombre);
+        return item.isPresent()? item.get(): new Item();
+    }
+
+    public List<Order> cargaOrder(String wizards_nombre) {
+        return orderRepo.findByUserName(wizards_nombre);
+    }
+
+    public Wizards creaWizards(Wizards wizards) {
+        wizardsRepo.persist(wizards);
+        return this.cargaWizards(wizards.getNombre());
+    }
+
+    public void eliminaWizards(String wizards_nombre) {
+        wizardsRepo.deleteById(wizards_nombre);
+    }
+
+    public List<Order> ordenes() {
+        return orderRepo.listAll();
+    }
+
+    // contenido min eval: if-else
+    @Transactional
+    public Order comanda(String wizards_nombre, String item_nombre) {
+        Order order = null;
+        Optional<Wizards> wizards = wizardsRepo.findByIdOptional(wizards_nombre);
+        Optional<MagicalItem> item = itemRepo.findByIdOptional(item_nombre);
+        if (wizards.isPresent() && item.isPresent() 
+            && wizards.get().getDestreza() >= item.get().getQuality()) {
+            order = new Order(wizards.get(), item.get());
+            orderRepo.persist(order);
+        }
+        return order;
+    }
+
+    // contenido min eval: loop, if-else, colecciones
+    @Transactional
+    public List<Order> comandaMultiple(String wizards, List<String> productos) {
+
+        Optional<Wizards> wiz = wizardsRepo.findByIdOptional(wizards);
+        if (wiz.isEmpty()) {
+            return Collections.emptyList();
+        }
+        
+        List<Order> orders = new ArrayList<Order>();
+
+        Order order = null;
+        for (String producto: productos) {
+            order = this.comanda(wiz.get().getNombre(), producto);
+            if (order != null) {
+                orders.add(order);
+            }
+        }
+        return orders;      
+    }
 }
