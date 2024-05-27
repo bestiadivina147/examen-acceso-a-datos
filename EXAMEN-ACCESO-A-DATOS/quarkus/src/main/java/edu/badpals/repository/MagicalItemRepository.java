c1f73a161e4f50fdfab9124ecf3bcd968fe6f515
bestiadivina147 <diegopc147@gmail.com>
2024-05-27 10:00:48 +0200

    build(MagicalItemRepository): add MagicalItemRepository

---
+++ EXAMEN-ACCESO-A-DATOS/quarkus/src/main/java/edu/badpals/repository/MagicalItemRepository.java
@@ -0,0 +1,8 @@
+
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.enterprise.context.ApplicationScoped;
+import edu.badpals.dominio.MagicalItem;
+
+public class MagicalItemRepository implements PanacheRepositoryBase<Item, String>{
+    
+}
