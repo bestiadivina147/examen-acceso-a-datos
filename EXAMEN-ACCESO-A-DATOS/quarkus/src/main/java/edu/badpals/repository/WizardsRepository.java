014f2cf5b85f54da01d0c32552d8f9d5bacd617b
bestiadivina147 <diegopc147@gmail.com>
2024-05-27 10:05:44 +0200

    build(WizardsRepository): add WizardsRepository

---
+++ EXAMEN-ACCESO-A-DATOS/quarkus/src/main/java/edu/badpals/repository/WizardsRepository.java
@@ -0,0 +1,8 @@
+
+import jakarta.enterprise.context.ApplicationScoped;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import edu.badpals.dominio.Wizards;
+
+public class WizardsRepository implements PanacheRepositoryBase<Wizards, String>{
+    
+}
