0e47849878168338c98cd395bd9cfecd3fdfc045
bestiadivina147 <diegopc147@gmail.com>
2024-05-27 10:07:35 +0200

    feat(ServiceOlli.java): add imports

--- EXAMEN-ACCESO-A-DATOS/quarkus/src/main/java/edu/badpals/ServiceOlli.java
+++ EXAMEN-ACCESO-A-DATOS/quarkus/src/main/java/edu/badpals/ServiceOlli.java
@@ -9,6 +9,9 @@ import jakarta.transaction.Transactional;
 import edu.badpals.dominio.MagicalItem;
 import edu.badpals.dominio.Wizards;
 import edu.badpals.dominio.Order;
+import edu.badpals.repository.MagicalItemRepository;
+import edu.badpals.repository.OrderRepository;
+import edu.badpals.repository.WizardsRepository;
 public class ServiceOlli {
     
 }
